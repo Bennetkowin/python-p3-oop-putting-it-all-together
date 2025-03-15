d5d113b250be9ad819417c1fa932bd18a9a0af85
Bennet Owino <bennetkowin09@gmail.com>
2025-03-16 01:19:02 +0300

    done

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,25 +1,20 @@
-#!/usr/bin/env python3
 
 class Shoe:
     def __init__(self, brand, size):
-        self.size = size
         self.brand = brand
-        
+        self.size = size
+
     @property
     def size(self):
-        return self.__sizeof__
-    
+        return self._size
+
     @size.setter
     def size(self, size):
         if isinstance(size, int):
             self._size = size
         else:
-            print("Size must be an interger")
-            
+            print("size must be an integer")
+
     def cobble(self):
+        print("Your shoe is as good as new!")
         self.condition = "New"
-        print("Your shoe is as good as new!")           
-    
-        
-        pass
-    pass
